96420fc2f3a5ef8acd37788ce7807165e37f199e
Ballantyne, Bob <bob.ballantyne@emc.com>
2018-05-02 20:40:47 -0400

    updates for CBF v3.0

--- build/action_folders/01.rt_environment/01.php
+++ build/action_folders/01.rt_environment/01.php
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 declare -ar env_php=(
+    'PHP=php7'
     'SESSIONS_DIR="${SESSIONS_DIR:-/sessions}"'
     'RUN_DIR="${RUN_DIR:-/run/php}"'
 )
 
-crf.updateRuntimeEnvironment "${env_php[*]}"
+crf.updateRuntimeEnvironment "${env_php[@]}"
--- build/usr/local/crf/startup/fcgi.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-sed -i "s|^socket=.*$|socket=unix://${RUN_DIR}/fcgiwrap.sock|"         /etc/supervisord.conf/fcgiwrap.ini
-sed -i "s|^\s*server\s+.*$|    server unix:${RUN_DIR}/fcgiwrap.sock;|" /etc/nginx/conf.d/fcgi.upstream
-
-crf.fixupDirectory "$RUN_DIR"
